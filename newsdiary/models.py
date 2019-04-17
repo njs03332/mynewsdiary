4c8c4d71b500a94780ff89600368c83e0ab45f5e
Yuri <njs03332@gmail.com>
2019-04-17 14:45:13 +0900

    model fixed

--- newsdiary/models.py
+++ newsdiary/models.py
@@ -8,11 +8,9 @@ from django.contrib.postgres.fields import ArrayField
 from django.utils import timezone
 from enum import Enum
 from polymorphic.models import PolymorphicModel
-from markdownx.models import MarkdownxField
 
 
 # 필수 아닌 것들 한 번 더 체크하기
-# 마크다운
 # 캘린더 API
 # seeding
 
@@ -55,6 +53,9 @@ class Event(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     datetime = models.DateTimeField()
     title = models.CharField(max_length=50)
+    concept = models.TextField(blank=True)
+    background = models.TextField(blank=True)
+    importance = models.TextField(blank=True)
     explanation = models.TextField(blank=True)  #markdown?
     category = models.CharField(max_length=20, choices=[(tag, tag.value) for tag in Category])
     issue = models.ForeignKey(Issue, on_delete=models.SET_NULL, related_name='events', blank=True, null=True)
@@ -68,10 +69,10 @@ class Article(models.Model):
     # author = models.CharField(max_length=20)
     press = models.CharField(max_length=20)
     title = models.CharField(max_length=50)
-    sub_title = models.CharField(max_length=50, blank=True)
-    content = MarkdownxField()
+    url = models.URLField(default="https://www.naver.com")
     issue = models.ForeignKey(Issue, on_delete=models.SET_NULL, related_name='articles', blank=True, null=True)
     event = models.ForeignKey(Event, on_delete=models.SET_NULL, related_name='articles', blank=True, null=True)
+    parent_event = models.ForeignKey(Event, on_delete=models.SET_NULL, related_name='top_article', blank=True, null=True)  # 어떤 event의 대표기사일 경우
 
     def __str__(self):
         return self.title
@@ -82,7 +83,7 @@ class Article(models.Model):
 
 class Journalist(models.Model):  # 그냥 매번 새로 생성. 이름만 들어가므로.. 기자의 기사들 모아보기 같은건 없다..
     name = models.CharField(max_length=20, blank=True)
-    email = models.EmailField(blank=True)
+    email = models.EmailField(max_length=100, blank=True)
     article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='authors')
 
 class Memo(models.Model):
@@ -92,6 +93,7 @@ class Memo(models.Model):
     updated_at = models.DateTimeField(blank=True, null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='memos', default=1)
     issue = models.ForeignKey(Issue, on_delete=models.SET_NULL, related_name='memos', null=True)  # 유저가 생성할 때 없는 건 불가능, 나중에 이슈가 삭제되어 표류하는 메모가 생겼을 때는 이 column이 null으로 존재 가능
+    article = models.ForeignKey(Article, on_delete=models.SET_NULL, related_name='memos', null=True)
 
     def update_date(self):
         self.updated_at = timezone.now()
@@ -100,11 +102,11 @@ class Memo(models.Model):
     def __str__(self):
         return self.title
 
-# class Noti(PolymorphicModel):
-#     timestamp = models.DateTimeField(auto_now_add=True)
-#     user = models.ForeignKey(User, on_delete=models.CASCADE)
-#     message = 
+class Noti(PolymorphicModel):
+    timestamp = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.CharField(max_length=100)
 
-# class NonregularNoti(Noti):
-#     issue = models.ForeignKey(Issue, on_delete=models.CASCADE, null=True, blank=True)
-#     event = models.ForeignKey(Event, on_delete=models.CASCADE, null=True, blank=True)
+class NonregularNoti(Noti):
+    issue = models.ForeignKey(Issue, on_delete=models.CASCADE, null=True, blank=True)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, null=True, blank=True)
